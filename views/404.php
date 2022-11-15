517fdabeb996de6bcf96c2c75809da805313bf4a
jacmill <jakub3020.jmy@gmail.com>
2022-11-15 20:27:16 +0100

    Added question and added return link to not found page

--- views/404.php
+++ views/404.php
@@ -5,7 +5,8 @@
 <body>
   <div class="wrapper">
     <h1 class="page__code">404</h1>
-    <p class="page__notfound">Nie znaleziono strony</p>
+    <p class="page__notfound">Nie znaleziono strony. Sprawdź czy dobrze wpisałeś link.</p>
+    <a href="/" class="page__return">Powrót na stronę główną</a>
   </div>
 </body>
 
